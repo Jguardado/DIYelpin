6c6164297b6f6590c967ba727470553e89f57141
Juan <jguardado85@gmail.com>
2016-01-25 19:28:32 -0800

    adding comments

--- client/CreatePost/createPostController.js
+++ client/CreatePost/createPostController.js
@@ -1,14 +1,23 @@
 angular.module('yelpin.createPost', [])
 
 .controller('createPostController', ['$scope', 'appFactory', 'sharedPropertyService', '$state', function($scope, appFactory, sharedPropertyService, $state) {
+  //the above module will require: ngfileUpload, this controller will also require: Upload, and $timeout.
+  //BELOW: captures the input values from the descritpion and text imput box.
+
   $scope.descript = '';
   $scope.txtcomment = '';
   $scope.category = '';
+
+  //using a function created in factory that stores the value of the username.
   var temp = sharedPropertyService.getProperty();
   console.log('this is the set property', temp);
 
   $scope.username = temp;
+
+  //this might not even be needed.
   $scope.comment = [];
+
+  //User authenticating uses the property in our factory and a check. If the property hasnt been changed then it will send you back to signin.
   $scope.checkAuth = function() {
     var check = sharedPropertyService.getProperty();
     console.log('check auth', check);
@@ -16,17 +25,22 @@ angular.module('yelpin.createPost', [])
       $state.go('signin');
     }
   };
+
+  //takes the values passed in and converts it in a data object being to be passed to our server
   $scope.postToPage = function() {
     console.log('this inside of post', $scope.username);
     if ($scope.txtcomment != '') {
       console.log($scope.txtcomment);
       console.log($scope.descript);
+      //this might not be needed
       $scope.comment.push($scope.txtcomment);
     }
 
     var data = { username: $scope.username, title: $scope.descript, message: $scope.txtcomment, category: $scope.category };
     console.log(data);
+    //uses the factory
     appFactory.setPost(data);
+    //resets the values to blank
     $scope.txtcomment = '';
     $scope.descript = '';
     $scope.category = '';
